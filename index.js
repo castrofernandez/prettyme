95d0b65cb4c186ee1a9c120e481db641bfcee005
Juan Castro Fernández <castrofernandez@gmail.com>
2018-04-08 17:34:34 +0200

    Adds load function.

--- index.js
+++ index.js
@@ -6,9 +6,29 @@ const htmlPrettier = require('./prettiers/html');
 
 var prettyme = (function() {
   var parser = htmlPrettier;
+  var selector = '.prettyme';
 
   function init(options) {
-    parser = options.parser;
+    options = options || {};
+
+    if (options.parser) {
+      parser = options.parser;
+    }
+    
+    if (options.selector) {
+      selector = options.selector;
+    }
+  }
+
+  function load() {
+    var previews = document.querySelectorAll(selector);
+    var length = previews.length;
+    var preview, i;
+
+    for (i = 0; i < length; i++) {
+      preview = previews[i];
+      preview.innerHTML = format(preview.innerHTML);
+    }
   }
 
   function parse(code) {
@@ -21,6 +41,7 @@ var prettyme = (function() {
 
   return {
     init: init,
+    load: load,
     parse: parse,
     format: format
   };
